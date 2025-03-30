5f2a35b2c509c9852c385b986d58a0c841b14dbc
tomguillermou <tomguillermou@gmail.com>
2025-03-30 16:26:35 +0200

    Implement not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+import { Header } from '@/components/layout/header'
+
+export default function Page() {
+  return (
+    <>
+      <Header />
+
+      <main className="p-8">
+        <h1 className="text-center text-2xl font-bold">
+          Oops! This page does not exist 🤷‍♂️
+        </h1>
+      </main>
+    </>
+  )
+}
